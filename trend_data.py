0f0157cf961aca5f530d17d4090a0681b6df5080
Vitaly Klimenko <vitostikitos@gmail.com>
2024-04-29 14:32:07 +0300

    Add an event and a trend data classes

---
+++ trend_data.py
@@ -0,0 +1,18 @@
+class TrendData:
+    def __init__():
+        '''
+        Инициализация класса
+        '''
+        pass
+
+    def from_str(string: str):
+        '''
+        Конструирует тренд дату из строки
+        '''
+        pass
+
+    def __str__(self) -> str:
+        '''
+        Превращает тренд дату в строку
+        '''
+        pass
